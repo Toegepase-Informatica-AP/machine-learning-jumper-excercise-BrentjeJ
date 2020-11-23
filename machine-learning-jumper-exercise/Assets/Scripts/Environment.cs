56cb3629cb7be6920750985f9637d8c2300d8fc6
Janssens Brent (s106999) <brent.janssens@student.ap.be>
2020-11-23 16:32:29 +0100

    Initial Commit

---
+++ machine-learning-jumper-exercise/Assets/Scripts/Environment.cs
@@ -0,0 +1,142 @@
+﻿using Assets.Scripts;
+using TMPro;
+using UnityEngine;
+
+public class Environment : MonoBehaviour
+{
+    public Obstacle obstaclePrefab;
+    public Player playerPrefab;
+    public float maxTime = 2f;
+    public float minTime = 1f;
+
+    private SpawnLine spawnLine;
+    private SpawnLineCross spawnLineCross;
+
+    private Player player;
+    private TextMeshPro scoreBoard;
+
+    private GameObject obstacles;
+    private GameObject obstaclesCross;
+
+    private Vector3 spawnLineLocation;
+    private Vector3 spawnLineLocationCross;
+
+    private GameObject floor;
+    private GameObject floorCross;
+
+    private Quaternion spawnLineRotation;
+    private Quaternion spawnLineRotationCross;
+
+    //current time
+    private float currentTimer;
+
+    //The time to spawn the object
+    private float randomTimed;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        currentTimer = 0f;
+        SetRandomTimed();
+    }
+
+    private void SetRandomTimed()
+    {
+        randomTimed = Random.Range(minTime, maxTime);
+    }
+
+    public void OnEnable()
+    {
+        player = transform.GetComponentInChildren<Player>();
+        scoreBoard = transform.GetComponentInChildren<TextMeshPro>();
+
+        //for default
+        obstacles = transform.Find("Obstacles").gameObject;             
+        spawnLine = transform.GetComponentInChildren<SpawnLine>();        
+        floor = transform.Find("Floor").gameObject;      
+
+        spawnLineLocation = spawnLine.transform.position;
+        spawnLineRotation = spawnLine.transform.rotation;
+
+        //for cross
+        obstaclesCross = transform.Find("ObstaclesCross").gameObject;
+        spawnLineCross = transform.GetComponentInChildren<SpawnLineCross>();
+        floorCross = transform.Find("FloorCross").gameObject;
+
+        spawnLineLocationCross = spawnLineCross.transform.position;
+        spawnLineRotationCross = spawnLineCross.transform.rotation;
+    }
+
+    public void ClearEnvironment()
+    {
+        foreach (Transform obstacle in obstacles.transform)
+        {
+            Destroy(obstacle.gameObject);
+        }
+    }
+
+    public void SpawnPlayer()
+    {
+        Player newPlayer = Instantiate(playerPrefab, new Vector3(0, 1.5f, 42.2f), new Quaternion(0f, 180f, 0f, 0f));
+        newPlayer.transform.SetParent(gameObject.transform);
+    }
+
+    private void SpawnObstacle()
+    {
+        ResetTimer();
+        //for default
+        Obstacle newObstacle = Instantiate(obstaclePrefab, new Vector3(spawnLineLocation.x, floor.transform.position.y + 0.5f, spawnLineLocation.z), spawnLineRotation);
+        newObstacle.transform.SetParent(obstacles.transform);
+        
+    }
+
+    private void SpawnObstacleCross()
+    {
+        ResetTimer();
+        //for cross
+        Obstacle newObstacleCross = Instantiate(obstaclePrefab, new Vector3(spawnLineLocationCross.x, floorCross.transform.position.y + 0.5f, spawnLineLocationCross.z), spawnLineRotationCross);
+        newObstacleCross.transform.SetParent(obstaclesCross.transform);
+    }
+
+    private void ResetTimer()
+    {
+        currentTimer = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        //Counts up
+        currentTimer += Time.deltaTime;
+        int randomValue = Random.Range(1, 3);
+
+        //Check if its the right time to spawn the object
+        if (CanSpawn())
+        {
+            switch (randomValue)
+            {
+                case 1:
+                    SpawnObstacle();
+                    break;
+                case 2:
+                    SpawnObstacleCross();
+                    break;
+                default:
+                    break;
+            }
+
+            RestartCurrentTimer();
+        }
+
+        scoreBoard.text = player.GetCumulativeReward().ToString("f3");
+    }
+
+    private bool CanSpawn()
+    {
+        return randomTimed <= currentTimer;
+    }
+
+    private void RestartCurrentTimer()
+    {
+        currentTimer = 0f;
+    }
+}
---
+++ machine-learning-jumper-exercise/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Obstacle : MonoBehaviour
+    {
+        public float givenSpeed = 5f;
+        public bool constantGivenSpeed = true;
+        public float minSpeed = 1f;
+        public float maxSpeed = 10;
+
+        private float randomizedSpeed = 0f;
+        private Environment environment;
+
+        public Player ply;
+
+        public void Start()
+        {
+            if (!constantGivenSpeed)
+            {
+                randomizedSpeed = Random.Range(minSpeed, maxSpeed);
+            }
+            else
+            {
+                randomizedSpeed = givenSpeed;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (randomizedSpeed > 0f)
+            {
+                Move();
+            }
+        }
+
+        private void Move()
+        {
+            if (environment == null)
+            {
+                environment = GetComponentInParent<Environment>();
+            }
+
+            transform.Translate(Vector3.forward * Time.deltaTime * randomizedSpeed);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            bool isDeadLineCollission = other.CompareTag("DeadLine");
+
+            bool isDeadLineCollissionCross = other.CompareTag("DeadLineCross");
+
+            if (isDeadLineCollission || isDeadLineCollissionCross)
+            {
+                Destroy(gameObject);
+                GameObject pl = GameObject.Find("Player");
+                Player ply = (Player)pl.GetComponent(typeof(Player));
+                ply.AddReward(1f);
+            }
+        }
+    }
+}
